d1b46d7d4bd544fdcbe9f5458dd07d8a18f5afd2
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-07 20:02:08 +0300

    Adds more testing routes
    Youtube link: https://www.youtube.com/watch?v=ZSshT_r9ihY
    Website link:
    https://nau4i.me/php/php-mvc-framework/376/mvc-framework-18-front-controller-%D1%80%D1%83%D1%82%D0%B8%D1%80%D0%B0%D0%BD%D0%B53

--- config/routes.php
+++ config/routes.php
@@ -3,6 +3,11 @@
 $cnf['administration']['namespace'] = 'Controllers\Admin';
 $cnf['administration']['controllers']['index'] = 'test';
 $cnf['administration']['controllers']['new'] = 'create';
+
+$cnf['admin']['namespace'] = 'Controllers\Admin2';
+
+$cnf['admin/users']['namespace'] = 'Controllers\Admin3';
+
 $cnf['*']['namespace'] = 'Controllers';
 
 return $cnf;
