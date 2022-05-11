314d3c503f2834a468fc1034740886287935ddb3
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-11 20:07:27 +0300

    Adds NativeSession class config and test code
    Youtube link: https://www.youtube.com/watch?v=U3HzDFCYMN4
    Website link:
    https://nau4i.me/php/php-mvc-framework/388/mvc-framework-30-native-session2

--- config/app.php
+++ config/app.php
@@ -3,5 +3,15 @@
 $cnf['default_controller'] = 'Index';
 $cnf['default_method'] = 'index';
 $cnf['namespaces']['Controllers'] = '../controllers/';
+
+$cnf['session']['autostart'] = true;
+$cnf['session']['type'] = 'native';
+$cnf['session']['name'] = '__sess';
+$cnf['session']['lifetime'] = 3600;
+$cnf['session']['path'] = '/';
+$cnf['session']['domain'] = '';
+$cnf['session']['secure'] = false;
+
+
 return $cnf;
 
--- public/index.php
+++ public/index.php
@@ -5,3 +5,5 @@ include '../../gf/App.php';
 $app = \GF\App::getInstance();
 $app->setRouter('JsonRPCRouter');
 $app->run();
+$app->getSession()->counter+=1;
+echo $app->getSession()->counter;
