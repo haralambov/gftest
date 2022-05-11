56b062e7edc55d403462578ccfc968fd7b1ac8b1
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-11 21:17:29 +0300

    Adds config to test DBSession class
    Youtube link: https://www.youtube.com/watch?v=nIa7KnhUQew
    Website link:
    https://nau4i.me/php/php-mvc-framework/389/mvc-framework-31-dbsession1

--- config/app.php
+++ config/app.php
@@ -5,13 +5,14 @@ $cnf['default_method'] = 'index';
 $cnf['namespaces']['Controllers'] = '../controllers/';
 
 $cnf['session']['autostart'] = true;
-$cnf['session']['type'] = 'native';
+$cnf['session']['type'] = 'database';
 $cnf['session']['name'] = '__sess';
 $cnf['session']['lifetime'] = 3600;
 $cnf['session']['path'] = '/';
 $cnf['session']['domain'] = '';
 $cnf['session']['secure'] = false;
-
+$cnf['session']['dbConnection'] = 'default';
+$cnf['session']['dbTable'] = 'sessions';
 
 return $cnf;
 
