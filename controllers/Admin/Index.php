5c7e0272a70edf7943c800d84934847880d2a48e
Zlatomir Haralambov <z.haralambov@gmail.com>
2022-05-12 21:34:54 +0300

    Tests the Validation class
    Youtube link: https://www.youtube.com/watch?v=Wo6iLUkJFfE
    Website link:
    https://nau4i.me/php/php-mvc-framework/394/mvc-framework-36-views4

--- controllers/Admin/Index.php
+++ controllers/Admin/Index.php
@@ -4,6 +4,17 @@ namespace Controllers\Admin;
 
 class Index {
     public function index() {
+
+        $val = new \GF\Validation();
+        $val->setRule('url', 'http://az.c@/', null, 'URL is not valid')
+            ->setRule('minlength', 'http://az.c/', 50, 'Min length is not valid');
+        echo $val->validate() ? 'Valid' : 'Not valid';
+        echo '<br>';
+        foreach ($val->getErrors() as $error) {
+            echo $error . '<br>';
+        }
+        echo '=========<br/>';
+
         $view = \GF\View::getInstance();
         $view->username = 'John Doe';
         $view->appendToLayout('body', 'admin.index');
